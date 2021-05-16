948f25d060485628a2f0c740efd334ebad4cf966
Sekara <sekara97@gmail.com>
2021-05-17 01:40:08 +0200

    Changed target framework to .net 5.0, added Ocelot API Gateway, changed gateway port

--- Gateway/Startup.cs
+++ Gateway/Startup.cs
@@ -3,10 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 namespace Gateway
 {
@@ -16,6 +14,7 @@ namespace Gateway
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOcelot();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,6 +24,8 @@ namespace Gateway
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseOcelot();
 
             app.UseRouting();
 
