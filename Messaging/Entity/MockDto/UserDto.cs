51f148ce70d6ef3ab0832c0bfa228b7b448b877c
bordjoski1 <jbordjoski@gmail.com>
2021-08-03 17:27:31 +0200

    User mock

---
+++ Messaging/Entity/MockDto/UserDto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Messaging.Entity.MockDto
+{
+    public class UserDto
+    {
+        /// <summary>
+        /// User ID
+        /// </summary>
+        public int UserId { get; set; }
+        /// <summary>
+        /// User name
+        /// </summary>
+        /// 
+        public string Name { get; set; }
+        /// <summary>
+        /// Last name of user
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// User address
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// User email
+        /// </summary>
+        public string Email { get; set;}
+
+        /// <summary>
+        /// User telephone
+        /// </summary>
+        public string Telephone { get; set; }
+
+        /// <summary>
+        /// User date of birth
+        /// </summary>
+        public DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// User description
+        /// </summary>
+        public string Description { get; set; }
+        
+    }
+}
