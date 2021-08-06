1da1dd4d279ce48f40441b64354c9f85207df6a9
milana998 <milana.maksimovic59@gmail.com>
2021-08-06 09:51:21 +0200

    Added entities and migrations

---
+++ Comments/Entities/Comment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Comments.Entities
+{
+    public class Comment
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public int CreatedByUserId { get; set; }
+
+        [Required, StringLength(100), MinLength(10)]
+        public string Text { get; set; }
+
+        [Required]
+        public int PostId { get; set; }
+
+        public IList<CommentReply> Replies{ get; set; }
+    }
+}
--- Comments/Startup.cs
+++ Comments/Startup.cs
@@ -1,7 +1,9 @@
+using Comments.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +34,9 @@ namespace Comments
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Comments", Version = "v1" });
             });
+
+            string ConnectionString = this.Configuration.GetConnectionString("Comments");
+            services.AddDbContext<CommentsContext>(options => options.UseSqlServer(ConnectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
